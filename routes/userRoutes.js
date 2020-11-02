51a2bab620d54f65aee2a146c08911f6af8feb67
mouadmaa <maaroufimouad00@gmail.com>
2020-11-02 15:01:26 +0100

    logout only authenticated users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,13 +7,14 @@ const router = Router()
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
-router.get('/logout', authController.logout)
 
 router.post('/refreshToken', authController.refreshToken)
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
 
 router.use(authController.protect)
+
+router.get('/logout', authController.logout)
 
 router.route('/me')
   .get(userController.getMe)
